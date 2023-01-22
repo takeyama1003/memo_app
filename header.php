9494df70374af84e6696f3e59fa26b35e89bfc6a
takeyama <creative1902@gmail.com>
2023-01-22 23:52:26 +0900

    header,footer,css追加

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>メモ帳アプリ</title>
+<link rel="stylesheet" href="../style.css">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
