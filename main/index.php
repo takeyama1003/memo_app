ffbaf1a6f41833a8185ee095e92949a5a276ca7c
takeyama <creative1902@gmail.com>
2023-01-26 02:01:54 +0900

    トップページのメニュー、検索回り修正

--- main/index.php
+++ main/index.php
@@ -1,8 +1,6 @@
-<?php session_start(); ?>
-<?php require '../header.php'; ?>
-<!-- <//?php  ?> -->
-<?php
-// unset($_SESSION['customer']);
+<?php 
+session_start();
+require '../header.php';
 require 'db-connect.php';
 $sql=$pdo->prepare('select * from customer where login=? and password=?');
 $sql->execute([$_REQUEST['login'], $_REQUEST['password']]);
@@ -11,46 +9,16 @@ foreach ($sql as $row) {
 		'id'=>$row['id'], 'name'=>$row['name'], 'login'=>$row['login'], 
 		'password'=>$row['password']];
 }
+
 require 'menu.php';
 
 if (isset($_SESSION['customer'])) {
-
-	echo '<form action="product.php" method="post">';
-
-	//検索
-	echo '<input type="text" name="keyword">';
-	echo '<input type="submit" value="検索">';
-	echo '</form>';
-	echo '<hr>';
-
-	echo 'こんにちは、', $_SESSION['customer']['name'], 'さん。';
-
+	require 'search.php';
+	//echo 'こんにちは、', $_SESSION['customer']['name'], 'さん。';
 	require 'category.php';
-
-	// echo '<table>';
-	// echo '<tr><th>ID</th><th>タイトル</th><th>本文</th><th>カテゴリー</th></tr>';
-	// $sql=$pdo->prepare(
-	// 	'select * from memo_category2 where memo_category2.customer_id=?');
-	// $sql->execute([$_SESSION['customer']['id']]);
-	// foreach ($sql as $row) {
-	// 	$id=$row['id'];
-	// 	echo '<tr>';
-	// 	echo '<td>', $id, '</td>';
-	// 	echo '<td>';
-	// 	echo '<a href="add-memo.php?id=', $id, '">', $row['name'], '</a>';
-	// 	echo '</td>';
-	// 	echo '<td>', $row['price'], '</td>';
-	// 	echo '<td>';
-	// 	echo '<a href="product.php?category_name=', $row['category_name'], '">', $row['category_name'], '</a>';
-	// 	echo '</td>';
-	// 	echo '</tr>';
-	// }
-	// echo '</table>';
-
 } else {
 	echo 'ログイン名またはパスワードが違います。';
 }
 ?>
-
 
 <?php require '../footer.php'; ?>
---
+++ main/search.php
@@ -0,0 +1,8 @@
+<div class="search">
+<?php
+	echo '<form action="product.php" method="post">';
+	echo '<input type="text" name="keyword">';
+	echo '<input type="submit" value="検索">';
+	echo '</form>';
+?>
+</div>
