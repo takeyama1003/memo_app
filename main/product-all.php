e134f8f3b6197d9e6e7ada503fc8bf4820e64132
takeyama <creative1902@gmail.com>
2023-01-29 16:16:53 +0900

    メモ修正

--- main/product-all.php
+++ main/product-all.php
@@ -1,6 +1,7 @@
+<section class="memo-all">
 <?php
 echo '<table>';
-echo '<tr><th>ID</th><th>タイトル</th><th>本文</th><th>カテゴリー</th><th></th></tr>';
+echo '<tr><th>タイトル</th><th>本文</th><th>カテゴリー</th><th></th></tr>';
 require 'db-connect.php';
 
 //$customer_idは配列
@@ -16,19 +17,16 @@ else if (isset($_REQUEST['category_name'])) {
 	$sql=$pdo->prepare("select * from memo_category2 where category_name like ? and memo_category2.customer_id='".$customer_id[0]."'");
 	$sql->execute(['%'.$_REQUEST['category_name'].'%']);
 
-	// $sql=$pdo->prepare('select * from memo_category2 where category_name like ?');
-	// $sql->execute(['%'.$_REQUEST['category_name'].'%']);
 }
 else {
 	//全表示
-	// $sql=$pdo->query('select * from memo_category2');
 	$sql=$pdo->prepare("select * from memo_category2 where memo_category2.customer_id= ?");
 	$sql->execute($customer_id);
 }
 foreach ($sql as $row) {
 	$id=$row['id'];
 	echo '<tr>';
-	echo '<td>', $id, '</td>';
+	// echo '<td>', $id, '</td>';
 	echo '<td>';
 	echo '<a href="add-memo.php?id=', $id, '">', $row['name'], '</a>';
 	// echo '<a href="detail.php?id=', $id, '">', $row['name'], '</a>';
@@ -45,3 +43,4 @@ foreach ($sql as $row) {
 }
 echo '</table>';
 ?>
+</section>
