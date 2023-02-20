a0b236de950994703ebbbe4b1b95d6f10c808092
takeyama <creative1902@gmail.com>
2023-02-21 00:36:04 +0900

    カテゴリー削除時に未分類にする

--- main/category-delete.php
+++ main/category-delete.php
@@ -7,6 +7,9 @@ if (!isset($_SESSION['customer'])) {
 <?php require '../header.php'; ?>
 <?php require 'menu.php'; ?>
 <?php
+//$customer_idは配列
+$customer_id=[$_SESSION['customer']['id']];
+
 if (isset($_SESSION['customer'])) {
 	require 'db-connect.php';
 
@@ -16,9 +19,17 @@ if (isset($_SESSION['customer'])) {
 		'delete from category where category_id=?');
 	$sql->execute([$id]);
 
+	//カスタマーID && 未分類
+	$sql3=$pdo->query("select * from category where category.customer_id='" .$customer_id[0]."'"." AND category_name = '未分類'");
+
+	//未分類のカテゴリーID格納
+	foreach ($sql3 as $row3) {
+		$categoryId = $row3['category_id'];
+	}
+
 	//未分類に設定
 	$sql2=$pdo->prepare(
-		"UPDATE product SET category_id = 10 WHERE category_id = $id ");
+		"UPDATE product SET category_id =".$categoryId." WHERE category_id = $id ");
 	$sql2->execute([$id]);
 
 	echo 'カテゴリーを削除しました。';
