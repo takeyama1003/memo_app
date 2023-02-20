385a969969b884b256123ba86ebc75ea8667f4cb
takeyama <creative1902@gmail.com>
2023-02-20 23:34:05 +0900

    jquery本体追加

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 <title>メモ帳アプリ</title>
 <link rel="stylesheet" href="../style.css">
 <link rel="stylesheet" href="style.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
 
