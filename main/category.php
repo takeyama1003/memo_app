6cbf2e9416fa460addd427a91ae3312824480179
takeyama <creative1902@gmail.com>
2023-02-21 01:07:20 +0900

    未分類の編集禁止

--- main/category.php
+++ main/category.php
@@ -5,24 +5,28 @@
 	$customer_id=[$_SESSION['customer']['id']];
 	//echo $customer_id[0];
 
-	$sql=$pdo->query("select * from category where category.customer_id='" .$customer_id[0]."'"." AND category_name != '未分類'");//カスタマーID && 未分類以外
+	$sql=$pdo->query("select * from category where category.customer_id='" .$customer_id[0]."'");//カスタマーID
 
 	//ファイル名取得(パス)
 	$dir = basename($_SERVER['SCRIPT_NAME']);
 
 	echo '<table>';
 	echo '<tr><th>カテゴリー</th>';
+
 	if($dir == "category-show.php"){
 		echo '<th></th><th></th>';
 	}
+
 	echo '</tr>';
+	
 	foreach ($sql as $row) {
 		$id=$row['category_id'];
 		echo '<tr>';
 		echo '<td>';
 		echo '<a href="product.php?category_name=', $row['category_name'], '">', $row['category_name'], '</a>';
 		echo '</td>';
-		if($dir == "category-show.php"){
+
+		if($dir == "category-show.php" && $row['category_name'] !== '未分類'){
 			echo '<td>';
 			echo '<a href="category-add.php?id=', $id, '">編集</a>';
 			echo '</td>';
@@ -30,6 +34,7 @@
 			echo '<a href="category-delete.php?id=', $id, '">削除</a>';
 			echo '</td>';
 		}
+
 		echo '</tr>';
 	}
 	echo '</table>';
